ae46d4ef8490c4d8909a8684efbf5bde91b96e3e
RishabhKumarGupta <35447048+RishabhKumarGupta@users.noreply.github.com>
2018-01-19 19:22:07 +0530

    Create largestthree.c

---
+++ largestthree.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int a;
+  int b;
+  int c;
+ 
+  printf("Input a  three no.\n");
+  scanf("%d", &a);
+  scanf("%d", &b);
+   scanf("%d", &c);
+  if (a>b && b>c){
+  printf("a is the largest ") ;}
+  if(b>c){
+      printf("b is largest");
+}else
+printf("c is largest");
+}
