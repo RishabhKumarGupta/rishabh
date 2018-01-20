71702b5c641faa5c3c7812a38680a2c936d95599
RishabhKumarGupta <35447048+RishabhKumarGupta@users.noreply.github.com>
2018-01-20 18:23:59 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+       int a,n,c;
+       printf(" enter the value:");
+       scanf("%d",&a);
+       printf(" enter the power:");
+       scanf("%d",&n);
+       	c=pow(a,n);
+       	printf("%d is the value of given number",c);
+       getch();
+}
