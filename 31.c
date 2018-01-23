ec2cba02e310ae1278fb7137cff2dc7aac25fab1
RishabhKumarGupta <35447048+RishabhKumarGupta@users.noreply.github.com>
2018-01-23 20:49:28 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{    char b[100];
+     int i,count=0;
+     scanf("%s",a);
+     for(i=0;b[i]!=NULL;i++){
+         if(b[i]!=' ')
+         count++;}
+         printf("%d",(count));
+     
+
+    return 0;
+}
