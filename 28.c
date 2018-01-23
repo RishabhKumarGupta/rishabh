2b8cf94a3d2eef06acb451a42b2924d828cfaf33
RishabhKumarGupta <35447048+RishabhKumarGupta@users.noreply.github.com>
2018-01-23 20:00:35 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main() {
+
+int arr[] = { 5, 8, 3, 7 };
+int Element = 3;
+int Index;
+
+for (int i = 0; i < 5; i++)
+{
+     if (Element == arr[i])
+     {
+         Index = i;
+     }
+}
+}
